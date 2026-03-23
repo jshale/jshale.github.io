16a06ad6a10280153b068efa1c5533ff1e0593d8
jake <jakehale44@gmail.com>
2026-03-23 17:52:53 -0500

    updates to functionality and styling

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -5,35 +5,9 @@ if ("serviceWorker" in navigator) {
       .then((reg) => console.log("SW registered:", reg.scope));
   });
 }
-
-//Handle trade form submission
-tradeform = document.getElementById("tradeform");
-tradeform.addEventListener("submit", function (e) {
-  //Prevent page reloads
+//Using the tabs to switch the visibility of the forms
+tab_button = document.querySelector(".tab_button.active_tab");
+tab_button.addEventListener("click", (e) => {
   e.preventDefault();
-
-  //Access the form data
-  const form_data = new FormData(this);
-
-  tradetype = form_data.get("trade_type");
-  result = form_data.get("result");
-  session_name = form_data
-    .get("session_name")
-
-    [
-      //Select all the items that match the button group names, then set them to false to reset the form
-      ("trade_type", "result")
-    ].forEach((name) => {
-      document.querySelectorAll(`input[name="${name}"]`).forEach((input) => {
-        input.checked = false;
-      });
-    });
-  //Select all the input fields and empty them on submission
-  document.querySelectorAll("input[type='number']").forEach((input) => {
-    input.value = "";
-  });
-
-  document.activeElement.blur();
-
-  console.log(tradetype, result);
+  console.log("tab clicked");
 });
---
+++ public/src/js/logging.js
@@ -0,0 +1,31 @@
+//Handle trade form submission
+tradeform = document.getElementById("tradeform");
+tradeform.addEventListener("submit", function (e) {
+  //Prevent page reloads
+  e.preventDefault();
+
+  //Access the form data
+  const form_data = new FormData(this);
+
+  tradetype = form_data.get("trade_type");
+  result = form_data.get("result");
+  session_name = form_data
+    .get("session_name")
+
+    [
+      //Select all the items that match the button group names, then set them to false to reset the form
+      ("trade_type", "result")
+    ].forEach((name) => {
+      document.querySelectorAll(`input[name="${name}"]`).forEach((input) => {
+        input.checked = false;
+      });
+    });
+  //Select all the input fields and empty them on submission
+  document.querySelectorAll("input[type='number']").forEach((input) => {
+    input.value = "";
+  });
+
+  document.activeElement.blur();
+
+  console.log(tradetype, result);
+});
