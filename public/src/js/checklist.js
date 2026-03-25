c2fe551a0435068295df1d22885fc9836fd0c7ad
jake <jakehale44@gmail.com>
2026-03-24 20:46:58 -0500

    refactored with alpine js

--- public/src/js/checklist.js
+++ public/src/js/checklist.js
@@ -1,17 +1,17 @@
 cond_button = document.querySelectorAll(".go_button");
 containers = document.querySelectorAll(".condition_container");
 
-cond_button.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("clicked");
-    element.classList.toggle("go_button_go");
-    element
-      .closest(".condition_container")
-      .classList.toggle("condition_container_go");
+// cond_button.forEach((element) => {
+//   element.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     console.log("clicked");
+//     element.classList.toggle("go_button_go");
+//     element
+//       .closest(".condition_container")
+//       .classList.toggle("condition_container_go");
 
-    element.textContent == "NO GO"
-      ? (element.textContent = "GO")
-      : (element.textContent = "NO GO");
-  });
-});
+//     element.textContent == "NO GO"
+//       ? (element.textContent = "GO")
+//       : (element.textContent = "NO GO");
+//   });
+// });
